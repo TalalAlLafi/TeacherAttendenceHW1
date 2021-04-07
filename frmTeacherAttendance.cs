6c92303dce7e9396086ec46dd79e1f46f37bb624
Talal Al-Lafi <allafi.talal@gmail.com>
2021-04-07 17:53:14 +0200

    first commit

---
+++ frmTeacherAttendance.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TeacherAttendance.helper;
+using TeacherAttendance.model;
+
+namespace TeacherAttendance
+{
+    public partial class frmTeacherAttendance : Form
+    {
+        private List<Attendance> attendancelist = new List<Attendance>();
+     
+        private AttendanceManagement attendance;
+        public frmTeacherAttendance()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmTeacherAttendance_Load(object sender, EventArgs e)
+        {
+           attendance = new AttendanceManagement();
+            ShowCourses();
+            ShowTeachers();
+            ShowRooms();
+        }
+
+        /*private void CmbCourses_SelectedValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CmbCourses_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }*/
+
+        private void ShowCourses()
+        {
+            cmbCourses.DataSource = null;
+            cmbCourses.DisplayMember = "CourseName";
+            cmbCourses.ValueMember = "CourseId";
+            cmbCourses.DataSource = attendance.getAllCourses();
+            cmbCourses.SelectedIndex = -1;
+        }
+
+        private void ShowTeachers()
+        {
+            cmbTeacherName.DataSource = null;
+            cmbTeacherName.DisplayMember = "TeacherName";
+            cmbTeacherName.ValueMember = "TeacherId";
+            cmbTeacherName.DataSource = attendance.getAllTeachers();
+            cmbTeacherName.SelectedIndex = -1;
+
+        }
+
+        private void ShowRooms()
+        {
+            cmbRoom.DataSource = null;
+            cmbRoom.DisplayMember = "RoomName";
+            cmbRoom.ValueMember = "RoomId";
+            cmbRoom.DataSource = attendance.getAllRooms();
+            cmbRoom.SelectedIndex = -1;
+
+        }
+        private void CmbCourses_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+
+
+            string value = "";
+
+
+            int id = ((Course)((ComboBox)sender).SelectedItem).CourseId;
+
+            if (id != 0)
+            {
+                return;
+            }
+
+            if (Prompt.InputBox("New course", "New course name:", ref value) == DialogResult.OK)
+            {
+                if (value.Trim().Length > 0)
+                {
+                    attendance.addNewCourse(value.Trim());
+                    ShowCourses();
+                }
+
+
+            }
+        }
+
+        private void CmbTeacherName_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+
+
+            string value = "";
+
+
+            int id = ((Teacher)((ComboBox)sender).SelectedItem).TeacherId;
+
+            if (id != 0)
+            {
+                return;
+            }
+
+            if (Prompt.InputBox("New teacher", "New teacher name:", ref value) == DialogResult.OK)
+            {
+                if (value.Trim().Length > 0)
+                {
+                    attendance.addNewTeacher(value.Trim());
+                    ShowTeachers();
+                }
+
+
+            }
+
+
+        }
+
+        private void CmbRoom_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            string value = "";
+
+
+            int id = ((Room)((ComboBox)sender).SelectedItem).RoomId;
+
+            if (id != 0)
+            {
+                return;
+            }
+
+            if (Prompt.InputBox("New Room/Lab", "New Room/Lab name:", ref value) == DialogResult.OK)
+            {
+                if (value.Trim().Length > 0)
+                {
+                    attendance.addNewRoom(value.Trim());
+                    ShowRooms();
+                }
+
+
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+           /* attendancelist.Add(new Attendance(cmbTeacherName, cmbCourses.SelectedText, cmbRoom.SelectedText,
+                dateTimePicker2.Text, dateTimePicker3.Text, textBox2.Text));*/
+
+            string selected = this.cmbTeacherName.GetItemText(this.cmbTeacherName.SelectedItem);
+            string sel2 = this.cmbRoom.GetItemText(this.cmbRoom.SelectedItem);
+          string sel3 = this.cmbCourses.GetItemText(this.cmbCourses.SelectedItem);
+            attendancelist.Add(new Attendance(selected, sel3, sel2,
+                dateTimePicker2.Text, dateTimePicker3.Text, textBox2.Text));
+
+          
+            dataGridView1.DataSource = attendancelist.ToList();
+
+        }
+    }
+}
+
+        
+
+     
+
---
+++ model/Attendance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherAttendance.model
+{
+    class Attendance
+    {
+
+        public string TeacherId { get; set; }
+        public string CourseId { get; set; }
+        public string RoomId { get; set; }
+        public String StartTime { get; set; }
+        public String LeaveTime { get; set; }
+        public String Comment { get; set; }
+
+        public Attendance(string TeacherId,string CourseId, string RoomId, String StartTime, String LeaveTime, String Comment)
+        {
+            this.TeacherId = TeacherId;
+            this.CourseId = CourseId;
+            this.RoomId = RoomId;
+            this.StartTime = StartTime;
+            this.LeaveTime = LeaveTime;
+            this.Comment = Comment;
+        }
+
+     
+    }
+}
